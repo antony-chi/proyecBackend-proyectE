45a8b905f99372fee53c995f9b69cb16df55a830
antony-chi <antony.cp9@gmail.com>
2023-04-13 19:01:43 -0500

    validar respuesta

--- src/controllers/reactivos.controller.js
+++ src/controllers/reactivos.controller.js
@@ -1,5 +1,7 @@
 import modelosInit from "../models/init-models.js"
 import { sequelize } from "../database/database.js";
+import { Op } from "sequelize";
+import respuestas from "../models/respuestas.js";
 let models = modelosInit(sequelize);
 
 //obtener reactivos
@@ -121,3 +123,39 @@ export const generarExamen = async (req,res) =>{
 const getRandomInt = (max) => {
     return Math.floor(Math.random() * max)
 }
+
+//obtenerRespuesta
+export const obtenerRespuesta = async (req,res) =>{
+    let consulta;
+    let {noReactivo,respuesta} = req.params;
+    try {
+        //para prevenir que se envie algo distinto a un numero es la api
+        let noReactivEsNumero = Number(noReactivo);
+        let siesnumero = noReactivEsNumero / noReactivEsNumero;
+        if(siesnumero != 1){
+            console.log("no es un numero")
+            res.status(500).json({"Eror": "Error iserte un numero de reactivo"})
+            return;
+        }
+        
+        consulta = await models.respuestas.findAll({
+            attributes: ["escorrecto"],
+            where: {
+                reactivo_id: noReactivo,
+                descripcion:{[Op.like]: `${respuesta}%`}
+            }
+        })
+        //validando que la consulta sea correcto consulta.length no sea vacio
+        if(consulta.length <= 0 ){
+            console.log("Error en la consulta, la pregunta o respuesta no es valido")
+            res.status(404).json({"Error": `el reactivo ${noReactivo} no se encotra o la respuesta /${respuesta} no existe en la base`})
+            return;
+        }
+    } catch (error) {
+        console.log("Hubo un error: " + error)
+        res.status(500).json({"Error": "Hubo un error, " + error})
+        return;
+    }
+
+    res.status(200).json(consulta);
+};
